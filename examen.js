6bebb29f272b91583ef657cc9e0d4460f2a4431d
Ilie <ischiopu48@boscdelacoma.cat>
2024-10-30 20:12:42 +0100

    tasca 1 finalitzada

--- examen.js
+++ examen.js
@@ -2,13 +2,17 @@
 function llegirProductes() {
     let productes; 
     let arrayObjectes = [];
+    let hiHaError = false;
     try { productes = fs.readFileSync('productes.json', 'utf-8');}
-    catch (error) { console.error(error);}
+    catch (error) { console.error(error); hiHaError = true;}
 
     producteObject = JSON.parse(productes);
 
-    for (let i = 0; i < producteObject.length; i++) {
-        let producte = producteObject[i];
-        arrayObjectes.push(producteJSON);
+    if (!hiHaError) {
+        for (let i = 0; i < producteObject.length; i++) {
+            let producte = producteObject[i];
+            arrayObjectes.push(producte);
+        }
     }
+    return arrayObjectes;
 }
